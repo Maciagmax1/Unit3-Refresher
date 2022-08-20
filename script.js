bab31c7151daf3d00658be4a2d9796a8b587f22b
Max Maciag <maciagmax1@gmail.com>
2022-08-20 19:07:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const firstName = "Matt";
+const lastName = "Wong";
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let population = 672351;
+console.log(population);
+population *= 3;
+console.log(population);
+
+const javaScriptIsCool = true;
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 4; i >= 0; i--) {
+  console.log(i);
+}
